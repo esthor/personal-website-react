ea8c87a14b695f0c58d1bb0491d606c6562e2e79
Erik Thorelli <ethorelli@gmail.com>
2018-06-04 06:37:07 -0700

    update jest tests on Content and Nav

--- src/nav/Nav.test.js
+++ src/nav/Nav.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Nav from './Nav';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Nav />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
